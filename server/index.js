c3eee6a03ecda96e4ac336d5b25f66267892d411
SakshamManav <manavsaksham06@gmail.com>
2025-09-07 21:10:35 +0530

    Added cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const axios = require('axios')
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://school-flow-a-real-time-educational-three.vercel.app"],
     credentials: true,
   })
 );
